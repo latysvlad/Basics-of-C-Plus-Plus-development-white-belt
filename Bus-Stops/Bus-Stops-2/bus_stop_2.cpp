49a98c08fb1fb9999aa29e4026a40365ac5ee471
Vlad Latyshev <152328059+latysvlad@users.noreply.github.com>
2023-11-29 14:04:18 +0300

    Add files via upload

---
+++ Bus-Stops/Bus-Stops-2/bus_stop_2.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string> 
+#include <vector> 
+#include <map> 
+ 
+using namespace std; 
+ 
+int main() { 
+ map<vector<string>, int> bus;
+ int q,n;
+ cin >> q;
+ for(int i=0;i<q;i++) 
+ {
+  vector<string> bus_stops;
+  cin>>n;
+  bus_stops.assign(n,"0");
+  for(auto& stop:bus_stops)
+  {
+  	cin>>stop;
+  }
+  if(bus.count(bus_stops)==0)
+  {
+  	int num=bus.size()+1;
+  	bus[bus_stops]=num;
+  	cout<<"New bus "<<bus[bus_stops];
+  }
+  else
+  {
+  	cout<<"Already exists for "<<bus[bus_stops];
+  }
+ } 
+ return 0; 
+}
