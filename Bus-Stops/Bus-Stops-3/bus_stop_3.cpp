9c4bdc6d6a1deb656aeb29b64e09f8fbeebb2369
Vlad Latyshev <152328059+latysvlad@users.noreply.github.com>
2023-11-29 14:07:17 +0300

    Add files via upload

---
+++ Bus-Stops/Bus-Stops-3/bus_stop_3.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string> 
+#include <set> 
+#include <map> 
+ 
+using namespace std; 
+ 
+int main() { 
+ map<set<string>, int> bus;
+ int q,n;
+ cin >> q;
+ for(int i=0;i<q;i++) 
+ {
+  cin>>n;
+  set<string> bus_stops;
+  for(int j=0;j<n;j++)
+  {
+	string stop;
+  	cin>>stop;
+  	bus_stops.insert(stop);
+  }
+  if(bus.count(bus_stops)==0)
+  {
+  	int num=bus.size()+1;
+  	bus[bus_stops]=num;
+  	cout<<"New bus "<<bus[bus_stops];
+  }
+  else
+  {
+  	cout<<"Already exists for "<<bus[bus_stops];
+  }
+ } 
+ return 0; 
+}
