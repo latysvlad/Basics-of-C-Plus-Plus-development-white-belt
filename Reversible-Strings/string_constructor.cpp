cf35a40628e88ca9f834cddb30dfd58dcb0cf54d
Vlad Latyshev <152328059+latysvlad@users.noreply.github.com>
2023-11-30 01:28:43 +0300

    Add files via upload

---
+++ Reversible-Strings/string_constructor.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+class ReversibleString{
+	public:
+	ReversibleString(){};
+	ReversibleString(const string& input){
+		str=input;
+	}
+	void Reverse(){
+		reverse(str.begin(),str.end());
+	}
+	string ToString()const{
+		return str;
+	}
+	
+	private:
+	string str;
+};
+
+int main() {
+ReversibleString s("live");
+s.Reverse();
+cout << s.ToString() << endl;
+s.Reverse();
+const ReversibleString& s_ref=s;
+string tmp = s_ref.ToString();
+cout << tmp << endl;
+ReversibleString empty;
+	cout<<'"'<<empty.ToString()<<'"';
+	
+	return 0;
+}
+ 
+
+	
