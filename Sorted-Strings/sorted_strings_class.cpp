90e9b3bc1a2a46b81c96a3e7d586613a4b096e15
Vlad Latyshev <152328059+latysvlad@users.noreply.github.com>
2023-11-30 01:13:51 +0300

    Add files via upload

---
+++ Sorted-Strings/sorted_strings_class.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class SortedStrings{
+public:
+	void AddString(const string& s){
+		DefaultStrings.push_back(s);
+	}
+	vector<string> GetSortedStrings(){
+		sort(begin(DefaultStrings),end(DefaultStrings));
+		return DefaultStrings;
+	}
+private:
+	vector<string> DefaultStrings;
+};
+
+void PrintSortedStrings(SortedStrings& strings){
+	for(const string& s:strings.GetSortedStrings()){
+		cout<<s<<' ';
+	}
+	cout<<endl;
+}
+	
+int main(){
+	SortedStrings strings;
+	strings.AddString("firtst");
+	strings.AddString("third");
+	strings.AddString("second");
+	PrintSortedStrings(strings);
+		
+	strings.AddString("second");
+	PrintSortedStrings(strings);
+		
+	return 0;
+}
+		
