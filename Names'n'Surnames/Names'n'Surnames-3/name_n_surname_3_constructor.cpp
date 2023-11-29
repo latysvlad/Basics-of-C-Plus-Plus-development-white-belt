2f9aeeb99e9d86faa2f4277bbbd9cbc22bbfa548
Vlad Latyshev <152328059+latysvlad@users.noreply.github.com>
2023-11-30 01:22:57 +0300

    Update name_n_surname_3_constructor.cpp

--- Names'n'Surnames/Names'n'Surnames-3/name_n_surname_3_constructor.cpp
+++ Names'n'Surnames/Names'n'Surnames-3/name_n_surname_3_constructor.cpp
@@ -188,6 +188,7 @@ int main(){
 	for (int year : {1959, 1960}) {
 		cout << person.GetFullNameWithHistory(year) << endl;
 	}
+	
 	person.ChangeFirstName(1965, "Appolinaria");
 	person.ChangeLastName(1967, "Ivanova");
 	for (int year : {1965, 1967}) {
